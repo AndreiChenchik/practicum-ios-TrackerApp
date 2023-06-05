5b5ff5750a646aa0b9a7a67a7cc3eedd427eb2f0
Andrei Chenchik <andrei@chenchik.me>
2023-06-05 21:51:50 +0200

    Show onboarding only once per install

--- TrackerApp/SceneDelegate.swift
+++ TrackerApp/SceneDelegate.swift
@@ -12,7 +12,14 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
 
         let window = UIWindow(windowScene: windowScene)
-        let home = HomeViewController(onboarding: OnboardingViewController())
+
+        var onboardingVC: OnboardingViewController?
+        if !UserDefaults.standard.bool(forKey: "isOnboardingShown") {
+            onboardingVC = .init()
+            UserDefaults.standard.setValue(true, forKey: "isOnboardingShown")
+        }
+
+        let home = HomeViewController(onboarding: onboardingVC)
 
         window.rootViewController = home
 
