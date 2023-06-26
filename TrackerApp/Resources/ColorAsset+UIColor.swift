8cb120a929001bc441bf5f6245d9a08a3d2292b6
Andrei Chenchik <andrei@chenchik.me>
2023-06-26 10:31:03 +0200

    Fix datePicker colors

--- TrackerApp/Resources/ColorAsset+UIColor.swift
+++ TrackerApp/Resources/ColorAsset+UIColor.swift
@@ -4,6 +4,7 @@ enum ColorAsset: String, CaseIterable {
     case black, blue, white, gray, lightGray, red, blackUniversal
     case contrast, background
     case statisticsGradient1, statisticsGradient2, statisticsGradient3
+    case datePickerBackground
 }
 
 extension UIColor {
